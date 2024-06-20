75f28e7f243154d1f3f167ea79cab0015005b21a
bill <1321531943@qq.com>
2024-06-20 14:48:39 +0800

    order添加接口

--- apps/order/rpc/internal/svc/servicecontext.go
+++ apps/order/rpc/internal/svc/servicecontext.go
@@ -1,9 +1,20 @@
 package svc
 
-import "github.com/billbliu/lebron/apps/order/rpc/internal/config"
+import (
+	"github.com/billbliu/lebron/apps/order/rpc/internal/model"
+	"github.com/billbliu/lebron/apps/product/rpc/productclient"
+	"github.com/billbliu/lebron/apps/user/rpc/userclient"
+
+	"github.com/billbliu/lebron/apps/order/rpc/internal/config"
+)
 
 type ServiceContext struct {
-	Config config.Config
+	Config         config.Config
+	OrderModel     model.OrdersModel
+	OrderitemModel model.OrderitemModel
+	ShippingModel  model.ShippingModel
+	UserRpc        userclient.User
+	ProductRpc     productclient.Product
 }
 
 func NewServiceContext(c config.Config) *ServiceContext {
